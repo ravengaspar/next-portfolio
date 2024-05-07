08f3f671b1042e754cc9076f2496aa37e87a894b
ravengaspar <ravengaspar@gmail.com>
2024-05-06 22:03:48 -0400

    Added Intro and About Me Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Intro from '@/components/intro'
 
 const inter = Inter({ subsets: ['latin'] })
 
